c06b7c00731d378995ebdd4c0cbf0fde7756bb53
Temi Adeoye <lade_adeoye@yahoo.com>
2024-03-09 23:46:30 +0000

    W3D1 - Incorporate index.erb for templating out the UI

--- app/actions.rb
+++ app/actions.rb
@@ -20,7 +20,7 @@ end
 # When a browser requests the root of the application
 get '/' do 
 
-  finstagram_post_shark = {
+  @finstagram_post_shark = {
     username: "sharky_j",
     avatar_url: "https://live.staticflickr.com/65535/52358606250_01c667c5da_w.jpg",
     photo_url: "https://live.staticflickr.com/65535/52358421508_786aa10e2c_c.jpg",
@@ -33,7 +33,7 @@ get '/' do
     }]
   }
 
-  finstagram_post_whale = {
+  @finstagram_post_whale = {
     username: "kirk_whalum",
     avatar_url: "https://live.staticflickr.com/65535/52358421348_f34c7996b1.jpg",
     photo_url: "https://live.staticflickr.com/65535/52357237337_1cc718f6a7_4k.jpg",
@@ -46,7 +46,7 @@ get '/' do
     }]
   }
 
-  finstagram_post_marlin = {
+  @finstagram_post_marlin = {
     username: "marlin_peppa",
     avatar_url: "https://live.staticflickr.com/65535/52358415933_0a0e6bc35f_3k.jpg",
     photo_url: "https://live.staticflickr.com/65535/52358494794_f88b160d15_4k.jpg",
@@ -56,15 +56,21 @@ get '/' do
     comments: [{
       username: "marlin_peppa",
       text: "lunchtime! ;)"
-    }]
+    }, {username: "tuna_t",
+    text: "Looks yummy!"}]
   }
 
-  [finstagram_post_shark, finstagram_post_whale, finstagram_post_marlin].to_s
+  @finstagram_posts = [@finstagram_post_shark, @finstagram_post_whale, @finstagram_post_marlin]
 
-  # finstagram_post[:comments][0][:username]
+  # This code will return "marlin_peppa"
+  #finstagram_post[:comments][0][:username]
 
 # Stop
+
+erb(:index)
+
 end
+
 
 # This option is not as great
 
